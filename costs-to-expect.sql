1af65654e879de7976b3e778f6e467860c76ea4b
Dean Blackborough <dean@g3d-development.com>
2019-09-18 10:14:02 +0100

    Updated schema
    - Update the schema to match v2.00 of the Costs to Expect API.
    - Minor updates to README and CHANGELOG

--- costs-to-expect.sql
+++ costs-to-expect.sql
@@ -1,6 +1,6 @@
 /*
-SQLyog Ultimate v13.1.1 (64 bit)
-MySQL - 5.7.21 : Database - costs_to_expect
+SQLyog Ultimate v13.1.5  (64 bit)
+MySQL - 5.6.39.0 : Database - costs_to_expect_api
 *********************************************************************
 */
 
@@ -12,9 +12,9 @@ MySQL - 5.7.21 : Database - costs_to_expect
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`costs_to_expect` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`costs_to_expect_api` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
-USE `costs_to_expect`;
+USE `costs_to_expect_api`;
 
 /*Table structure for table `category` */
 
@@ -22,12 +22,30 @@ DROP TABLE IF EXISTS `category`;
 
 CREATE TABLE `category` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `resource_type_id` bigint(20) unsigned NOT NULL DEFAULT '1',
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `category_name_unique` (`name`)
+  UNIQUE KEY `category_name_resource_type_id_unique` (`name`,`resource_type_id`),
+  KEY `category_resource_type_id_foreign` (`resource_type_id`),
+  CONSTRAINT `category_resource_type_id_foreign` FOREIGN KEY (`resource_type_id`) REFERENCES `resource_type` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Table structure for table `error_log` */
+
+DROP TABLE IF EXISTS `error_log`;
+
+CREATE TABLE `error_log` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `message` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `file` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `line` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `trace` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Table structure for table `item` */
@@ -37,16 +55,17 @@ DROP TABLE IF EXISTS `item`;
 CREATE TABLE `item` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `resource_id` bigint(20) unsigned NOT NULL,
-  `description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `effective_date` date NOT NULL,
-  `total` decimal(10,2) NOT NULL,
-  `percentage` tinyint(3) unsigned NOT NULL DEFAULT '100',
-  `actualised_total` decimal(10,2) NOT NULL,
+  `created_by` bigint(20) unsigned NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
+  `updated_by` bigint(20) unsigned DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `item_resource_id_foreign` (`resource_id`),
-  CONSTRAINT `item_resource_id_foreign` FOREIGN KEY (`resource_id`) REFERENCES `resource` (`id`)
+  KEY `item_created_by_foreign` (`created_by`),
+  KEY `item_updated_by_foreign` (`updated_by`),
+  CONSTRAINT `item_created_by_foreign` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`),
+  CONSTRAINT `item_resource_id_foreign` FOREIGN KEY (`resource_id`) REFERENCES `resource` (`id`),
+  CONSTRAINT `item_updated_by_foreign` FOREIGN KEY (`updated_by`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Table structure for table `item_category` */
@@ -77,10 +96,83 @@ CREATE TABLE `item_sub_category` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `item_sub_category_item_category_id_foreign` (`item_category_id`),
+  UNIQUE KEY `item_sub_category_item_category_id_unique` (`item_category_id`),
   KEY `item_sub_category_sub_category_id_foreign` (`sub_category_id`),
   CONSTRAINT `item_sub_category_item_category_id_foreign` FOREIGN KEY (`item_category_id`) REFERENCES `item_category` (`id`),
   CONSTRAINT `item_sub_category_sub_category_id_foreign` FOREIGN KEY (`sub_category_id`) REFERENCES `sub_category` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Table structure for table `item_type` */
+
+DROP TABLE IF EXISTS `item_type`;
+
+CREATE TABLE `item_type` (
+  `id` int(3) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `item_type_name_index` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Table structure for table `item_type_allocated_expense` */
+
+DROP TABLE IF EXISTS `item_type_allocated_expense`;
+
+CREATE TABLE `item_type_allocated_expense` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `item_id` bigint(20) unsigned NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `effective_date` date NOT NULL,
+  `publish_after` date DEFAULT NULL,
+  `total` decimal(10,2) NOT NULL,
+  `percentage` tinyint(3) NOT NULL,
+  `actualised_total` decimal(10,2) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `effective_date` (`effective_date`),
+  KEY `publish_after` (`publish_after`),
+  KEY `item_id` (`item_id`),
+  CONSTRAINT `item_type_allocated_expense_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Table structure for table `item_type_simple_expense` */
+
+DROP TABLE IF EXISTS `item_type_simple_expense`;
+
+CREATE TABLE `item_type_simple_expense` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `item_id` bigint(20) unsigned NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `effective_date` date NOT NULL,
+  `total` decimal(10,2) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `effective_date` (`effective_date`),
+  KEY `item_id` (`item_id`),
+  CONSTRAINT `item_type_simple_expense_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Table structure for table `permitted_user` */
+
+DROP TABLE IF EXISTS `permitted_user`;
+
+CREATE TABLE `permitted_user` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `resource_type_id` bigint(20) unsigned NOT NULL,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `permitted_users_resource_type_id_foreign` (`resource_type_id`),
+  KEY `permitted_users_user_id_foreign` (`user_id`),
+  CONSTRAINT `permitted_user_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `permitted_users_resource_type_id_foreign` FOREIGN KEY (`resource_type_id`) REFERENCES `resource_type` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Table structure for table `request_error_log` */
@@ -90,12 +182,15 @@ DROP TABLE IF EXISTS `request_error_log`;
 CREATE TABLE `request_error_log` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `method` char(8) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `source` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'api',
+  `debug` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `expected_status_code` smallint(5) unsigned NOT NULL,
   `returned_status_code` smallint(5) unsigned NOT NULL,
   `request_uri` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `request_error_log_source_index` (`source`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Table structure for table `request_log` */
@@ -105,10 +200,12 @@ DROP TABLE IF EXISTS `request_log`;
 CREATE TABLE `request_log` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `method` char(8) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `source` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'api',
   `request` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `request_log_source_index` (`source`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Table structure for table `resource` */
@@ -134,12 +231,31 @@ DROP TABLE IF EXISTS `resource_type`;
 
 CREATE TABLE `resource_type` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `public` tinyint(1) NOT NULL DEFAULT '0',
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `resource_type_name_unique` (`name`)
+  UNIQUE KEY `resource_type_name_unique` (`name`),
+  KEY `resource_type_public_index` (`public`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Table structure for table `resource_type_item_type` */
+
+DROP TABLE IF EXISTS `resource_type_item_type`;
+
+CREATE TABLE `resource_type_item_type` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `resource_type_id` bigint(20) unsigned DEFAULT NULL,
+  `item_type_id` int(3) unsigned DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `resource_type_id` (`resource_type_id`),
+  KEY `item_type_id` (`item_type_id`),
+  CONSTRAINT `resource_type_item_type_ibfk_1` FOREIGN KEY (`resource_type_id`) REFERENCES `resource_type` (`id`),
+  CONSTRAINT `resource_type_item_type_ibfk_2` FOREIGN KEY (`item_type_id`) REFERENCES `item_type` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Table structure for table `sub_category` */
